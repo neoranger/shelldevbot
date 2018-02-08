77779e9ed66945ce1af90d0b54c183a804dc62ce
NeoRanger <neoranger@disroot.org>
2018-02-08 20:27:11 +0000

    new file:   runbot.sh

---
+++ runbot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+while true
+do
+python2 /home/pi/Documents/git/neobot/shelldevbot.py
+echo "¡The bot is crashed!"
+echo "Rebooting in:"
+for i in 1
+do
+echo "$i..."
+done
+echo "###########################################"
+echo "#Bot is restarting now                    #"
+echo "###########################################"
+done
